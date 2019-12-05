c5e9a1cf7a5e21c1a402639976a93bb323508f2b
yannis97 <yan.arg18@gmail.com>
2019-12-05 11:24:33 +0100

    change TestJulien for JUnit4

--- src/test/java/JulienTest.java
+++ src/test/java/JulienTest.java
@@ -7,17 +7,15 @@ import java.io.File;
 import org.junit.Test;
 
 public class JulienTest {
-
 	
 	@Test
 	public void dataForJulien()
 	{
     	//int nb=new File(".\\src\\main\\java\\game\\entity\\mob").listFiles().length;
     	File myFile[] = new File(".\\src\\main\\java\\game\\entity\\mob").listFiles();
-    	assertEquals((myFile.length-2),8);
-//    	System.out.println(new File(".\\src\\main\\java\\game\\entity\\mob").listFiles().clone()[1]);
+    	//System.out.println(new File(".\\src\\main\\java\\game\\entity\\mob").listFiles().clone()[1]);
     	System.out.println(new File(".\\src\\main\\java\\game\\entity\\mob").listFiles());
+    	assertEquals((myFile.length-2),8);
 	}
 	
-
 }
