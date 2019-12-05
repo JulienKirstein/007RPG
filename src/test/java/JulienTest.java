1dd5eb3560c90d4be1996f07f78c6ac5e8a4d133
yannis97 <yan.arg18@gmail.com>
2019-12-05 11:35:37 +0100

    update julienTest

--- src/test/java/JulienTest.java
+++ src/test/java/JulienTest.java
@@ -12,7 +12,7 @@ public class JulienTest {
     	//int nb=new File(".\\src\\main\\java\\game\\entity\\mob").listFiles().length;
     	File myFile[] = new File(".\\src\\main\\java\\game\\entity\\mob").listFiles();
     	//System.out.println(new File(".\\src\\main\\java\\game\\entity\\mob").listFiles().clone()[1]);
-    	System.out.println(new File(".\\src\\main\\java\\game\\entity\\mob").listFiles());
+    	//System.out.println(new File(".\\src\\main\\java\\game\\entity\\mob").listFiles());
     	assertEquals((myFile.length-2),8);
 	}
 	
