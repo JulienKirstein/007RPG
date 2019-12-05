f831f83aef6cf93863c04279315341bcdaf53185
yannis97 <yan.arg18@gmail.com>
2019-12-05 11:17:53 +0100

    Create JulienTest class for J.G

---
+++ src/test/java/JulienTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JulienTest {
+
+	@Test
+	public void testDataJuGo() {
+		fail("Not yet implemented");
+	}
+
+}
