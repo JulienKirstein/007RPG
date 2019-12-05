a3d3a52629aaaf14410ef49a63d9f48938f4eadb
yannis97 <yan.arg18@gmail.com>
2019-12-05 10:24:01 +0100

    update game test

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,22 +1,23 @@
 package test.java;
-
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
-
 import main.java.game.Game;
-
 import java.util.Scanner;
-
 class GameTest {
-
-    public static  Game game;
 
 	@Test
 	void createGameTest() throws InterruptedException
 	{
 		Scanner sc = new Scanner("Yannis");
-    	game = Game.getInstance(sc);
+    	Game game = Game.getInstance(sc);
     	assertEquals(game==null,false);
 	}
+	
+	@Test
+	void loadingTest()
+	{
+		int result = Game.loading();
+    	assertEquals(result,1);
+	}
+	
 }
